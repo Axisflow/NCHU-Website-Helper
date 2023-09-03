5afa4f81befc1158a84f6d29182c6518e301cb74
Axisflow <admin@axisflow.biz>
2023-09-04 01:26:26 +0800

    Add 'iLearning Helper for Loginning'

--- Portal-Login/pl.meta.js
+++ Portal-Login/pl.meta.js
@@ -3,14 +3,14 @@
 // @namespace    https://idp.nchu.edu.tw/
 // @version      0.1.10
 // @description  興大入口自動登入
-// @homepage     https://www.nchu.edu.tw/
+// @homepage     https://github.com/Axisflow
 // @author       Axisflow
 // @match        https://idp.nchu.edu.tw/*
 // @icon         https://www.nchu.edu.tw/favicon.ico
 // @updateURL    https://axisflow.github.io/NCHU-Website-Helper/Portal-Login/pl.meta.js
 // @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/Portal-Login/pl.user.js
+// @supportURL   https://github.com/Axisflow/NCHU-Website-Helper/issues
 // @grant        unsafeWindow
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @grant        window.close
 // ==/UserScript==

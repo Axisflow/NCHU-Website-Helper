b2af1ed75192aeb5cf2f47616884b2c475d1013c
Axisflow <admin@axisflow.biz>
2023-09-04 11:01:22 +0800

    fix 'cannot execute script on iLearning'

--- Portal-Login/pl.user.js
+++ Portal-Login/pl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NCHU Portal Helper for Loginning
 // @namespace    https://idp.nchu.edu.tw/
-// @version      0.1.10
+// @version      0.1.11
 // @description  興大入口自動登入
 // @homepage     https://github.com/Axisflow
 // @author       Axisflow
@@ -11,8 +11,6 @@
 // @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/Portal-Login/pl.user.js
 // @supportURL   https://github.com/Axisflow/NCHU-Website-Helper/issues
 // @grant        unsafeWindow
-// @grant        GM_setValue
-// @grant        GM_getValue
 // ==/UserScript==
 
 (function () {
--- iLearning-Login/il.meta.js
+++ iLearning-Login/il.meta.js
@@ -1,16 +1,14 @@
 // ==UserScript==
 // @name         NCHU iLearning Helper for Loginning
 // @namespace    https://lms2020.nchu.edu.tw/
-// @version      0.1.1
 // @description  興大 iLearning 3.0 自動登入
 // @homepage     https://github.com/Axisflow
+// @version      0.1.2
 // @author       Axisflow
-// @match        https:/lms2020.nchu.edu.tw/*
+// @match        https://lms2020.nchu.edu.tw/*
 // @icon         https://lms2020.nchu.edu.tw/sysdata/siteInfo/5def_16x16.png
 // @updateURL    https://axisflow.github.io/NCHU-Website-Helper/iLearning-Login/il.meta.js
 // @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/iLearning-Login/il.user.js
 // @supportURL   https://github.com/Axisflow/NCHU-Website-Helper/issues
-// @grant        unsafeWindow
-// @grant        GM_setValue
-// @grant        GM_getValue
+// @grant        none
 // ==/UserScript==

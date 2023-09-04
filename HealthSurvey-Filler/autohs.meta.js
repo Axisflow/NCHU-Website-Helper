ed895ffe5b4d49e3a173f0eb35f95713a3068c8f
Axisflow <admin@axisflow.biz>
2023-09-04 12:42:04 +0800

    Add 'Health Form Filler'

---
+++ HealthSurvey-Filler/autohs.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         NCHU Health Form Filler
+// @namespace    https://avenger2.nchu.edu.tw/HealthSurvey/
+// @version      0.1.4
+// @description  自動幫你填寫疫苗表單
+// @homepage     https://github.com/Axisflow
+// @author       Axisflow
+// @match        https://avenger2.nchu.edu.tw/*
+// @icon         https://www.nchu.edu.tw/favicon.ico
+// @updateURL    https://axisflow.github.io/NCHU-Website-Helper/Auto-NCHU-HealthSurvey/autohs.meta.js
+// @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/Auto-NCHU-HealthSurvey/autohs.user.js
+// @supportURL   https://github.com/Axisflow/NCHU-Website-Helper/issues
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        window.close
+// ==/UserScript==

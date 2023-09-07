2e3d6893c2b1f9fbf3d70b9344cd4e7a294a6e9e
Axisflow <admin@axisflow.biz>
2023-09-07 20:29:57 +0800

    Add 'Helper for Logging in'

---
+++ AllPortals-Login/apl.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         NCHU Helper for Logging in
+// @namespace    https://www.nchu.edu.tw/
+// @description  興大系統入口自動填寫驗證碼
+// @homepage     https://github.com/Axisflow
+// @version      0.1.5
+// @author       Axisflow
+// @match        https://lms2020.nchu.edu.tw/*
+// @match        https://onepiece.nchu.edu.tw/ps/plsql/*
+// @match        https://onepiece2-sso.nchu.edu.tw/ps/plsql/*
+// @match        https://onepiece.nchu.edu.tw/cofsys/plsql/*
+// @match        https://onepiece2-sso.nchu.edu.tw/cofsys/plsql/*
+// @icon         https://lms2020.nchu.edu.tw/sysdata/siteInfo/5def_16x16.png
+// @updateURL    https://axisflow.github.io/NCHU-Website-Helper/AllPortals-Login/apl.meta.js
+// @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/AllPortals-Login/apl.user.js
+// @supportURL   https://github.com/Axisflow/NCHU-Website-Helper/issues
+// @grant        none
+// ==/UserScript==
